bdfdfe23fc606ce189c33407724c92df283c1927
Stefan Schinkel <mail@dreeg.org>
2024-05-01 17:50:39 +0200

    start splitting config is dedicated files

---
+++ lua/bindings.lua
@@ -0,0 +1,8 @@
+--[[
+    KEYBINDINGS for nvim
+--]]
+-- TIP: Disable arrow keys in normal mode
+vim.keymap.set('n', '<left>', '<cmd>echo "use h to move!!"<cr>')
+vim.keymap.set('n', '<right>', '<cmd>echo "use l to move!!"<cr>')
+vim.keymap.set('n', '<up>', '<cmd>echo "use k to move!!"<cr>')
+vim.keymap.set('n', '<down>', '<cmd>echo "use j to move!!"<cr>')
