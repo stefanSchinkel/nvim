2fa6c4fc57a99687f68c930e64a7109e69ea3ea8
Stefan Schinkel <mail@dreeg.org>
2024-10-04 09:14:38 +0200

    [fix] fix deprecations in which-key

--- init.lua
+++ init.lua
@@ -153,19 +153,18 @@ require('lazy').setup({
       require('which-key').setup()
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+      require('which-key').add {
+        { '', desc = '', hidden = true, mode = { 'n', 'n', 'n', 'n', 'n', 'n', 'n' } },
+        { '', group = '[C]ode' },
+        { '', group = '[D]ocument' },
+        { '', group = 'Git [H]unk' },
+        { '', group = '[R]ename' },
+        { '', group = '[T]oggle' },
+        { '', group = '[S]earch' },
+        { '', group = '[W]orkspace' },
       }
       -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
+      require('which-key').add { '<leader>h', desc = 'Git [H]unk', mode = 'v' }
     end,
   },
   { -- Fuzzy Finder (files, lsp, etc)
