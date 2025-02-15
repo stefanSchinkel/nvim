a983f811448f8ab6c823ef0f754555086e7f8251
Stefan Schinkel <mail@dreeg.org>
2025-02-15 12:23:08 +0100

    [mouse] allow mouse/arrow key move again

--- lua/bindings.lua
+++ lua/bindings.lua
@@ -2,10 +2,10 @@
     KEYBINDINGS for nvim
 --]]
 -- TIP: Disable arrow keys in normal mode
-vim.keymap.set('n', '<left>', '<cmd>echo "use h to move!!"<cr>')
-vim.keymap.set('n', '<right>', '<cmd>echo "use l to move!!"<cr>')
-vim.keymap.set('n', '<up>', '<cmd>echo "use k to move!!"<cr>')
-vim.keymap.set('n', '<down>', '<cmd>echo "use j to move!!"<cr>')
+-- vim.keymap.set('n', '<left>', '<cmd>echo "use h to move!!"<cr>')
+-- vim.keymap.set('n', '<right>', '<cmd>echo "use l to move!!"<cr>')
+-- vim.keymap.set('n', '<up>', '<cmd>echo "use k to move!!"<cr>')
+-- vim.keymap.set('n', '<down>', '<cmd>echo "use j to move!!"<cr>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
