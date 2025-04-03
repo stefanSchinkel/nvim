2b94cb73bb4c46d86b92f2736e7ae7e8af46e4cd
Stefan Schinkel <mail@dreeg.org>
2025-04-03 08:43:26 +0200

    [pyrun] save file before running

--- lua/bindings.lua
+++ lua/bindings.lua
@@ -29,7 +29,7 @@ vim.keymap.set('n', '<space>', 'za', { desc = 'Fold at current line' })
 vim.keymap.set('n', '<C-t>', '<Cmd>Neotree toggle<CR>', { desc = 'Toggle Neotree' })
 
 -- code runner
-vim.keymap.set('n', '<leader>b', ':RunFile<CR>', { desc = 'Run file', noremap = true, silent = false })
+vim.keymap.set('n', '<leader>b', ':w<CR> :RunFile<CR>', { desc = 'Run file', noremap = true, silent = false })
 
 -- FTerm
 vim.keymap.set('n', '<leader>t', '<CMD>lua require("FTerm").toggle()<CR>')
