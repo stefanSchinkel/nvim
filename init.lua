846b28816b326d4e543a088c531599c6492cdd9f
Stefan Schinkel <mail@dreeg.org>
2026-09-07 14:01:27 +0200

    [tree-sitter] switch to main

--- init.lua
+++ init.lua
@@ -614,39 +614,34 @@ require('lazy').setup({
       end
     end,
   },
-  { -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    branch = 'master',
-    build = ':TSUpdate',
-    opts = {
-      ensure_installed = { 'gitcommit', 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
-    config = function(_, opts)
-      -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-      -- Prefer git instead of curl in order to improve connectivity in some environments
-      require('nvim-treesitter.install').prefer_git = true
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
-    end,
-  },
-  {
-    'iamcco/markdown-preview.nvim',
-    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    ft = { 'markdown' },
-    build = function()
-      vim.fn['mkdp#util#install']()
-    end,
-  },
+{ -- Highlight, edit, and navigate code
+  'nvim-treesitter/nvim-treesitter',
+  branch = 'main',
+  lazy = false,       -- main does not support lazy-loading
+  build = ':TSUpdate',
+  config = function()
+    require('nvim-treesitter').setup()
+
+    -- replaces ensure_installed
+    require('nvim-treesitter').install {
+      'gitcommit', 'bash', 'c', 'html', 'lua', 'luadoc',
+      'markdown', 'markdown_inline', 'vim', 'vimdoc',
+    }
+
+    -- replaces highlight = { enable = true }
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function(args)
+        -- skip ruby (was in additional_vim_regex_highlighting / indent disable)
+        if vim.bo[args.buf].filetype == 'ruby' then return end
+        local ok = pcall(vim.treesitter.start, args.buf)
+        if ok then
+          -- replaces indent = { enable = true } (experimental)
+          vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+      end,
+    })
+  end,
+},
   -- FTerm
   { 'numToStr/FTerm.nvim' },
   -- Code Runner ( <leader> to run
